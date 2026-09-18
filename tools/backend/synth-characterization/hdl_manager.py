f227d6f9d07f2e615f518b49f1a0ecad8a7b19c4
Endri Bezati <endri.bezati@gmail.com>
2026-09-18 16:46:31 +0200

    autopep8 on the characterization tool

--- tools/backend/synth-characterization/hdl_manager.py
+++ tools/backend/synth-characterization/hdl_manager.py
@@ -1,6 +1,7 @@
 # This script is used to move the HDL files in the hdl_out_dir and generate the necessary HDL files for the unit.
 import os
 
+
 def copy_dependency_rtl_files(dependencies, dependency_dict, hdl_out_dir, dynamatic_dir):
     """
     Copy the RTL files of the dependencies of the given unit to the output directory.
@@ -44,12 +45,13 @@ def copy_dependency_rtl_files(dependencies, dependency_dict, hdl_out_dir, dynama
         if "_dataless" in extra_dependency:
             os.system(f"cp {extra_rtl} {hdl_out_dir}/{rtl_filename.replace('.vhd', '_dataless.vhd')}")
         else:
-            os.system(f"cp {extra_rtl} {hdl_out_dir}")  
+            os.system(f"cp {extra_rtl} {hdl_out_dir}")
+
 
 def get_hdl_files(unit_name, generic, generator, dependencies, hdl_out_dir, dynamatic_dir, dependency_dict, module_name=None):
     """
     Generate or copy the HDL files for the given unit.
-    
+
     Args:
         unit_name (str): Name of the unit.
         generic (str): Generic information for the unit.
@@ -85,11 +87,11 @@ def get_hdl_files(unit_name, generic, generator, dependencies, hdl_out_dir, dyna
         cmd = generator.replace("$DYNAMATIC", dynamatic_dir).replace("$OUTPUT_DIR", hdl_out_dir).replace("$MODULE_NAME", simplified_unit_name).replace("$PREDICATE", "ne")
         # If a generator is provided, run the generator command
         print(f"Running generator command: {cmd}")
-        os.system(cmd)        
+        os.system(cmd)
         # Assert that the RTL file was generated
         rtl_file_vhdl = f"{hdl_out_dir}/{simplified_unit_name}.vhd"  # Assuming
         rtl_file_verilog = f"{hdl_out_dir}/{simplified_unit_name}.v"
         assert os.path.exists(rtl_file_vhdl) or os.path.exists(rtl_file_verilog), f"RTL file for unit {unit_name} was not generated or copied successfully."
         rtl_file = rtl_file_vhdl if os.path.exists(rtl_file_vhdl) else rtl_file_verilog
-    
+
     return rtl_file
